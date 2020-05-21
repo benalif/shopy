50954351b2a8b2e41c97ef22762e5bde97633189
benalif <farid.benali89@gmail.com>
2020-05-21 11:15:39 +0100

    correct forbidden http status in customAccesDeniedHandler

--- src/main/java/dz/shopy/loginservice/security/CustomAccessDeniedHandler.java
+++ src/main/java/dz/shopy/loginservice/security/CustomAccessDeniedHandler.java
@@ -1,6 +1,6 @@
 package dz.shopy.loginservice.security;
 
-import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 
 import java.io.IOException;
 
@@ -18,7 +18,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
-		this.writeResponse(response, SC_UNAUTHORIZED, "Forbidden");
+		this.writeResponse(response, SC_FORBIDDEN, "Forbidden");
 
 	}
 
